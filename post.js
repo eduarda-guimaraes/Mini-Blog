7ba168e0e2d61b16b2d7f7f39b1c63e930aad1b5
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2025-05-23 15:50:00 -0300

    Sugestão de post

--- post.js
+++ post.js
@@ -1,13 +1,20 @@
 angular.module('blog').controller('PublicacaoController', function($scope, $http) {
   const params = new URLSearchParams(window.location.search);
-  const index = params.get("index");
+  const index = parseInt(params.get("index"));
 
   $scope.publicacao = null;
+  $scope.publicacoesSugeridas = [];
 
   $http.get('https://api-fake-blog.onrender.com/postagens')
     .then(function(response) {
       const publicacoes = response.data;
       $scope.publicacao = publicacoes[index] || null;
+
+      // Guarda o índice original junto com o objeto
+      $scope.publicacoesSugeridas = publicacoes
+        .map((p, i) => ({ ...p, index: i }))
+        .filter((p) => p.index !== index)
+        .slice(0, 3);
     })
     .catch(function(error) {
       console.error("Erro ao buscar publicações:", error);
